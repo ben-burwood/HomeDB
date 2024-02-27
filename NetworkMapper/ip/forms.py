be40deadc01ab7bd74ebbaff503accb33b1921bc
Ben Burwood <ben.burwood@outlook.com>
2024-02-27 22:10:19 +0000

    Form and CRUD for ClientDevices
    Including VLAN/WiFi Select Options

--- NetworkMapper/ip/forms.py
+++ NetworkMapper/ip/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from .models import VLAN, WifiNetwork
+from .models import ClientDevice, VLAN, WifiNetwork
 
 class VLANForm(forms.ModelForm):
     class Meta:
@@ -12,3 +12,23 @@ class WifiForm(forms.ModelForm):
     class Meta:
         model = WifiNetwork
         fields = ["ssid", "password"]
+
+
+class ClientDeviceForm(forms.ModelForm):
+
+    connection_type = forms.ChoiceField(choices=ClientDevice.CONNECTION_TYPES, widget=forms.Select(attrs={"class": "form-control"}))
+
+    class Meta:
+        model = ClientDevice
+        fields = ["name", "mac_address", "ip_address", "wifi", "vlan", "connection_type"]
+
+    def __init__(self, *args, vlan_options=None, wifi_options=None, **kwargs):
+        super(ClientDeviceForm, self).__init__(*args, **kwargs)
+
+        empty_choice = [("", "None")]
+
+        self.fields["vlan"].widget = forms.Select(choices=empty_choice + vlan_options, attrs={"class": "form-control"})
+        self.fields["vlan"].required = False
+
+        self.fields["wifi"].widget = forms.Select(choices=empty_choice + wifi_options, attrs={"class": "form-control"})
+        self.fields["wifi"].required = False
--- NetworkMapper/ip/urls.py
+++ NetworkMapper/ip/urls.py
@@ -11,4 +11,8 @@ urlpatterns = [
     path("wifi/create", views.wifi.create, name="wifi.create"),
     path("wifi/<int:id>/edit", views.wifi.edit, name="wifi.edit"),
     path("wifi/<int:id>/delete", views.wifi.delete, name="wifi.delete"),
+    path("device", views.device.index, name="device.index"),
+    path("device/create", views.device.create, name="device.create"),
+    path("device/<int:id>/edit", views.device.edit, name="device.edit"),
+    path("device/<int:id>/delete", views.device.delete, name="device.delete"),
 ]
