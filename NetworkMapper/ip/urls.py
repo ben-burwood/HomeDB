fccda151fc699e84e13ec6d9af643f54cc17e6c2
Ben Burwood <ben.burwood@outlook.com>
2024-02-27 19:23:50 +0000

    Add Create Form/Template for VLANs

--- NetworkMapper/ip/urls.py
+++ NetworkMapper/ip/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("vlan", views.index, name="vlan.index"),
+    path("vlan/create", views.create_vlan, name="vlan.create"),
 ]
--- NetworkMapper/ip/views.py
+++ NetworkMapper/ip/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
+from .forms import VLANForm
 from .models import VLAN
+
 
 def index(request):
     vlans = VLAN.objects.all()
     return render(request, "ip/vlan/index.html", {"vlans": vlans})
+
+
+def create_vlan(request):
+    if request.method == "POST":
+        form = VLANForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("vlan.index")  # Redirect to the VLAN Index after successful creation
+    else:
+        form = VLANForm()
+
+    return render(request, "ip/vlan/create.html", {"form": form})
