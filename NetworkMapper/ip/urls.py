31df4075b547504f2ae52071e2181843767add4a
Ben Burwood <ben.burwood@outlook.com>
2024-02-27 20:49:09 +0000

    Add Wifi Views/Urls/Migrations/Form

--- NetworkMapper/ip/urls.py
+++ NetworkMapper/ip/urls.py
@@ -3,7 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("vlan", views.index, name="vlan.index"),
+    path("vlan", views.vlan_index, name="vlan.index"),
     path("vlan/create", views.create_vlan, name="vlan.create"),
     path("vlan/<int:vlan_id>/edit", views.edit_vlan, name="vlan.edit"),
+    path("wifi", views.wifi_index, name="wifi.index"),
+    path("wifi/create", views.create_wifi, name="wifi.create"),
+    path("wifi/<int:id>/edit", views.edit_wifi, name="wifi.edit"),
 ]
--- NetworkMapper/ip/views.py
+++ NetworkMapper/ip/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import get_object_or_404, redirect, render
 
-from .forms import VLANForm
-from .models import VLAN
+from .forms import VLANForm, WifiForm
+from .models import VLAN, WifiNetwork
 
-def index(request):
+def vlan_index(request):
     vlans = VLAN.objects.all()
     return render(request, "ip/vlan/index.html", {"vlans": vlans})
 
@@ -32,3 +32,34 @@ def edit_vlan(request, vlan_id: int):
         form = VLANForm(instance=vlan)
 
     return render(request, "ip/vlan/edit.html", {"form": form, "vlan": vlan})
+
+
+def wifi_index(request):
+    wifis = WifiNetwork.objects.all()
+    return render(request, "ip/wifi/index.html", {"wifis": wifis})
+
+
+def create_wifi(request):
+    if request.method == "POST":
+        form = WifiForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("wifi.index")  # Redirect to the VLAN Index after successful creation
+    else:
+        form = WifiForm()
+
+    return render(request, "ip/wifi/create.html", {"form": form})
+
+
+def edit_wifi(request, id: int):
+    wifi = get_object_or_404(WifiNetwork, id=id)
+
+    if request.method == "POST":
+        form = WifiForm(request.POST, instance=wifi)
+        if form.is_valid():
+            form.save()
+            return redirect("wifi.index")  # Redirect to the VLAN Index after successful edit
+    else:
+        form = WifiForm(instance=wifi)
+
+    return render(request, "ip/wifi/edit.html", {"form": form, "wifi": wifi})
