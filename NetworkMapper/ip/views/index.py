f0fd772e56f1389f8c85137cf391ed40374905de
Ben Burwood <ben.burwood@outlook.com>
2024-02-28 20:35:58 +0000

    Implement Writing YAML Config

--- NetworkMapper/ip/views/index.py
+++ NetworkMapper/ip/views/index.py
@@ -1,22 +1,32 @@
 import yaml
 from django.conf import settings
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.urls import reverse
+
+
+def update_config_value(key: str, value: str) -> None:
+    with open(settings.CONFIG_FILE_PATH, "r+") as yaml_file:
+        yaml_data = yaml.safe_load(yaml_file) or {}
+        yaml_data[key] = value
+        yaml_file.seek(0)
+        yaml.dump(yaml_data, yaml_file)
+        yaml_file.truncate()
 
 
 def index(request):
-    with open(settings.CONFIG_FILE_PATH, "r") as yaml_file:
-        yaml_data = yaml.safe_load(yaml_file)
-        if not yaml_data:
-            yaml_data = {}
-    base_ip = yaml_data.get("base_ip", "0.0")
-    router_ip = yaml_data.get("router_ip", "0.0.0.0")
-
     if request.method == "POST":
         if "base_ip-submit" in request.POST:
             base_ip = request.POST.get("base_ip")
-            # Logic for handling Form 1 submission
+            update_config_value("base_ip", base_ip)
+            return redirect(reverse("index"))
         elif "router_ip-submit" in request.POST:
             router_ip = request.POST.get("router_ip")
-            # Logic for handling Form 2 submission
+            update_config_value("router_ip", router_ip)
+            return redirect(reverse("index"))
+
+    with open(settings.CONFIG_FILE_PATH, "r") as yaml_file:
+        yaml_data = yaml.safe_load(yaml_file) or {}
+    base_ip = yaml_data.get("base_ip", "0.0")
+    router_ip = yaml_data.get("router_ip", "0.0.0.0")
 
     return render(request, "ip/index.html", {"base_ip": base_ip, "router_ip": router_ip})
