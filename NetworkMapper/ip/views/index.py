7c1a723c217b58c0343b4e383015f3ebd0f4fb9f
Ben Burwood <ben.burwood@outlook.com>
2024-02-28 20:24:13 +0000

    Implement Reading YAML Config

--- NetworkMapper/ip/views/index.py
+++ NetworkMapper/ip/views/index.py
@@ -1,9 +1,15 @@
+import yaml
+from django.conf import settings
 from django.shortcuts import render
 
 
 def index(request):
-    base_ip = request.POST.get("base_ip", "0.0")
-    router_ip = request.POST.get("router_ip", "0.0.0.0")
+    with open(settings.CONFIG_FILE_PATH, "r") as yaml_file:
+        yaml_data = yaml.safe_load(yaml_file)
+        if not yaml_data:
+            yaml_data = {}
+    base_ip = yaml_data.get("base_ip", "0.0")
+    router_ip = yaml_data.get("router_ip", "0.0.0.0")
 
     if request.method == "POST":
         if "base_ip-submit" in request.POST:
